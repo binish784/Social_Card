7c03edccb061e5ed255f8625dfda27883d7425f4
binish784 <binish784@gmail.com>
2019-05-06 19:56:20 +0545

    Minor CHanges

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import SocialCard from './components/card';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div>
-      <SocialCard title='The Practical Dev' description='Learning React ? Start Small.'/>
+      <SocialCard description="Learn by doing, you dont have to be great to start but start to be great." title='The Practical Dev' author='binish784' subTitle='Learning React ? Start Small.'/>
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import {FaHeart,FaShare,FaRegComments} from 'react-icons/fa';
 
 class SocialCard extends Component { 
 
     state={
+        'show':false,
+        'comments':[
+            'Default Comment',
+            'Second Comment'
+        ],
         'like':50,
-        'comment':15,
+        'comment':0,
         'share':5,
         'liked':false,
         'shared':false,
         'commented':false,
+        'comment_text':'',
     }
 
     handleLike=(e)=>{
@@ -31,59 +38,117 @@ class SocialCard extends Component {
     }
     
     handleShare=(e)=>{
-        let share=this.state.share;
-        share=share+1;
-        this.setState({share});
+        if(!this.state.shared){
+            let share=this.state.share;
+            let shared=this.state.shared;
+            share=share+1;
+            shared=true;
+            this.setState({share,shared});
+        }
     }
+
+    handleChange=(e)=>{
+        let comment_text=e.target.value;
+        this.setState({comment_text});
+    }
+
+    addComment=()=>{
+        let comments=this.state.comments;
+        let comment_text=this.state.comment_text;
+        comments.push(comment_text);
+        console.log(comment_text);
+        comment_text="";
+        this.setState({comment_text}); 
+    }
+
+    handleKeyPress=(e)=>{
+        if(e.key==="Enter"){
+            let comments=this.state.comments;
+            let comment_text=this.state.comment_text;
+            if(comment_text==''){
+                return false;
+            }else{
+                comments.push(comment_text);
+                comment_text="";
+                this.setState({comment_text}); 
+            }
+        }
+    }
+
+    showShare(){
+        if(this.state.shared){
+            return <div className='col-sm-4 alignCenter padding5'><a href='javascript:void(0)' className='textShared nodecoration'><span style={{color:'deepskyblue'}}><FaShare size={20} />&nbsp;&nbsp; {this.state.share} </span></a></div>              
+        }else{
+            return <div className='col-sm-4 alignCenter padding5'><a href='javascript:void(0)' className='nodecoration' onClick={this.handleShare}><FaShare size={20} />&nbsp;&nbsp; {this.state.share} </a></div>             
+        }
+    }
+
 
     showLike(){
         if(this.state.liked){
-            return(<div className='col-sm-4 alignCenter padding5 '><a href="#" className='textLoved' onClick={this.handleLike}>{this.state.comment} Loved</a></div>);
+            return(<div className='col-sm-4 alignCenter padding5 '><a href="javascript:void(0)" className='textLoved nodecoration' onClick={this.handleLike}><span style={{color:'red'}}><FaHeart size={20}/>&nbsp;{this.state.like}  </span></a></div>);
         }else{
-            return(<div className='col-sm-4 alignCenter padding5'><a href="#" onClick={this.handleLike}>{this.state.comment} Love </a></div>);
+            return(<div className='col-sm-4 alignCenter padding5'><a href="javascript:void(0)" className=' nodecoration' onClick={this.handleLike}><span ><FaHeart size={20}/>&nbsp;  {this.state.like}</span></a></div>);
         }
     }
 
     render(){
        return (
+           <div className='container'>
            <div className='row'>
           
-                <div className='col-md-6 offset-md-3'>
+                <div className='col-md-8'>
                 <hr></hr>
                     <div className='row'>
                         <div className='col-sm-2 nopadding'>
                            <img src='https://img.icons8.com/color/1600/circled-user-male-skin-type-1-2.png' className='imageIcon'></img>
                         </div>
                             <div className='col-sm-8 nopadding'>
-                            <b>The Practical Dev</b> <a href="#" className='greyText'>@ThePracticalDev</a> <br></br>
-                            Learning React ? Start small.<br></br>
-                            {'{ author :  '} <a href="#profile">@binish784</a> { '}'}
+                            <b>{this.props.title}</b> <a href="javascript:void(0)" className='greyText'>@ThePracticalDev</a> <br></br>
+                            {this.props.subTitle}<br></br>
+                            {'{ author :  '} <a href="javascript:void(0)" className='nodecoration'>@{this.props.author}</a> { '}'}
                             <br></br>
                             
-                            <a href='#' className='colorBlack'><div className='card'>
+                            <a href='javascript:void(0)' className='colorBlack'><div className='card'>
                                 <img src='https://images.alphacoders.com/511/thumb-1920-511052.jpg' className='postImage card-img-top img-fluid'></img>
                                 <div className='card-body'>
                                
                                     <b className='card-title'>
-                                    Learning React ? Start small.
+                                    {this.props.subTitle}
                                     </b>
                                     <p className='smallText'>
-                                        Learn by doing, you dont have to be great to start but start to be great.
+                                        {this.props.description}
                                     </p>
                                 </div>
                             </div></a>
 
                             <div className='row'>
                                 {this.showLike()}
-                                <div className='col-sm-4 alignCenter padding5'><a href="#" onClick={this.handleComment}>{this.state.comment} Comment</a></div>
-                                <div className='col-sm-4 alignCenter padding5'><a href='#' onClick={this.handleShare}>{this.state.share} Share</a></div>
+                                <div className='col-sm-4 alignCenter padding5'><a href="javascript:void(0)" className='nodecoration' onClick={this.handleComment}>{this.state.comments.length}&nbsp;&nbsp; <FaRegComments size={20}/></a></div>
+                                {this.showShare()}
                             </div>
                             
+                          
                             </div>
                     </div>
                     
                 <hr></hr>
                 </div>
+                <div className='col-md-4'>
+                <br></br>
+                <p className='commentHeader'>Comments:</p>
+                
+                <div>
+                    <input type='text' placeholder='Comment Here...' onKeyPress={this.handleKeyPress} onChange={this.handleChange} value={this.state.comment_text}></input>
+                    &nbsp;&nbsp;&nbsp;<input type='submit' value='Comment' onClick={this.addComment}></input>
+                </div>
+                <br></br>
+                {this.state.comments.map((val,key)=>
+                    <p key={key}>{val}</p>
+                    )}
+                </div>
+            </div>
+
             </div>
        );
    }
